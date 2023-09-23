24963a8e790523c63aa78208f39287cc578a8aa7
EmmanuelAdu <christorenzo20@gmail.com>
2023-09-23 19:25:27 +0100

    Update to header file

--- sort.h
+++ sort.h
@@ -24,11 +24,12 @@ typedef struct listint_s
 /*alx helper functions prototype*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void swap_int(int *first_int, int *second_int);
 
 /*project helper functions*/
-
+void bubble_sort(int *array, size_t size);
+void swap_int(int *first_int, int *second_int);
+listint_t *create_listint(const int *array, size_t size);
+void insertion_sort_list(listint_t **list)
 
 
 #endif
